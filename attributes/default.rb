ebebfdf9082f80087f5cec1cca2b1c85082a773a
Chance Zibolski <chance.zibolski@gmail.com>
2013-08-28 16:51:50 -0700

    Initial re-write to not use application cookbooks

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,25 @@
 default['ganeti_webmgr']['name'] = "ganeti_webmgr"
-default['ganeti_webmgr']['path'] = "/home/vagrant/ganeti_webmgr"
+default['ganeti_webmgr']['path'] = path = "/home/vagrant/gwm"
+default['ganeti_webmgr']['packages'] = ['git-core']
+default['ganeti_webmgr']['pip_packages'] = []
+default['ganeti_webmgr']['virtualenv'] = "#{path}/venv"
+default['ganeti_webmgr']['synced_folder'] = nil
 
-default['ganeti_webmgr']['user'] = "ganeti_webmgr"
-default['ganeti_webmgr']['group'] = "ganeti_webmgr"
+default['ganeti_webmgr']['owner'] = "vagrant"
+default['ganeti_webmgr']['group'] = "vagrant"
 default['ganeti_webmgr']['host'] = node['fqdn']
 default['ganeti_webmgr']['port'] = 8000
+
+default['ganeti_webmgr']['requirements'] = "requirements/prod.txt"
+
+default['ganeti_webmgr']['database']['engine'] = engine = "mysql"
+db = Chef::DataBagItem.load("database", engine)
+# Load the DB Credentials using the databag as defaults.
+default['ganeti_webmgr']['database']['name'] = db['name']
+default['ganeti_webmgr']['database']['user'] = db['user']
+default['ganeti_webmgr']['database']['password'] = db['password']
+default['ganeti_webmgr']['database']['host'] = db['host']
+default['ganeti_webmgr']['database']['port'] = db['port']
 
 default['ganeti_webmgr']['collectstatic_dir'] = "#{default['ganeti_webmgr']['path']}/shared/collected_static"
 
--- recipes/default.rb
+++ recipes/default.rb
@@ -8,42 +8,124 @@
 #
 
 app = node['ganeti_webmgr']
+db = app['database']
+venv = app["virtualenv"]
 
-application app['name'] do
-  path app['path']
-  owner "vagrant"
-  group "vagrant"
-  repository "https://github.com/osuosl/ganeti_webmgr"
-  revision "feature/14625"
-  migrate true
-  # rollback_on_error false
-  packages ["git-core"]
+include_recipe "python"
 
-  django do
-    requirements "requirements/prod.txt"
-    debug true
-    local_settings_file "ganeti_web/ganeti_web/settings/end_user.py"
-    settings_template "end_user.py.erb"
-    settings :app => app
-    collectstatic true
-    manage_file "ganeti_web/manage.py"
-    database do
-      database "#{app.path}/shared/ganeti.db"
-      engine "sqlite3"
+log "Creating project directory at #{app.path}"
+directory app.path do
+  owner app.owner
+  group app.group
+  mode 0744
+  action :create
+end
+
+log "Installing system packages for #{app.name}"
+app['packages'].each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+
+project_location = ::File.join(app.path, app.name)
+if node.chef_environment == "vagrant"
+  if app.synced_folder.nil?
+    raise "Node attribute synced_folder must be set."
+  else
+    log "Using Vagrant, symlinking project at #{project_location}"
+    link project_location do
+      to app.synced_folder
     end
+  end
+else
+  # Non vagrant
+end
 
-    venv = "#{app.path}/shared/env"
-    manage_cmd = "#{::File.join(venv, "bin", "python")} #{manage_file}"
-    syncdb_cmd = "#{manage_cmd} syncdb --noinput"
-    migration_command "#{syncdb_cmd} && #{manage_cmd} migrate"
+log "Creating Virtualenv"
+python_virtualenv venv do
+  path venv
+  owner app.owner
+  group app.group
+  action :create
+end
+
+# include proper recipes and install the db driver
+db_pip_packages = case node['ganeti_webmgr']['database']['engine']
+  when "mysql"
+    include_recipe "mysql"
+    ['mysql-python']
+  when "postgresql_psycopg2"
+    include_recipe "postgres"
+    ['psycopg2']
+  when "sqlite3"
+    include_recipe "sqlite"
   end
 
-  gunicorn do
-    app_module 'ganeti_web.wsgi:application'
-    port app['port']
-    directory "#{::File.join(app.path, "current", "ganeti_web")}"
+log "Installing pip packages"
+# Join the two lists of packages together (no dupes)
+python_pkgs = app.pip_packages | db_pip_packages
+python_pkgs.each do |pkg|
+  python_pip pkg do
+    virtualenv venv
+    user app.owner
+    group app.group
+    action :install
   end
+end
 
+requirements = ::File.join(project_location, app.requirements)
+log "Installing requirements.txt from #{requirements}"
+python_pip requirements do
+    virtualenv venv
+    options "-r"
+    user app.owner
+    group app.group
+    action :install
 end
 
 include_recipe "ganeti_webmgr::proxy"
+
+# application app['name'] do
+#   path app['path']
+#   owner "vagrant"
+#   group "vagrant"
+#   repository "https://github.com/osuosl/ganeti_webmgr"
+#   revision "feature/14625"
+#   migrate true
+#   rollback_on_error false
+#   packages ["git-core"] | app['packages']
+
+
+#   django do
+#     requirements "requirements/prod.txt"
+#     debug true
+#     local_settings_file "ganeti_web/ganeti_web/settings/end_user.py"
+#     settings_template "end_user.py.erb"
+#     settings :app => app
+#     collectstatic true
+#     manage_file "ganeti_web/manage.py"
+#     database do
+#       database db['name']
+#       engine db['engine']
+#       user db['user']
+#       password db['password']
+#       host db['host']
+#       port db['port']
+#     end
+
+#     manage_cmd = "#{::File.join(venv, "bin", "python")} #{manage_file}"
+#     syncdb_cmd = "#{manage_cmd} syncdb --noinput"
+#     migration_command "#{syncdb_cmd} && #{manage_cmd} migrate"
+
+#   end
+
+#   gunicorn do
+#     app_module 'ganeti_web.wsgi:application'
+#     port app['port']
+#     directory "#{::File.join(app.path, "current", "ganeti_web")}"
+#   end
+#
+# end
+
