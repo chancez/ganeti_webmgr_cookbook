b4f13def1acef141877fe965f296bb677bcf3421
Chance Zibolski <chance.zibolski@gmail.com>
2013-10-29 13:15:14 -0700

    Add license to top of recipes

--- recipes/bootstrap_user.rb
+++ recipes/bootstrap_user.rb
@@ -1,3 +1,20 @@
+#
+# Cookbook Name:: ganeti_webmgr
+# Recipe:: boostrap_user
+#
+# Copyright 2013 OSU Open Source Lab
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+
 python_bin = ::File.join(node['ganeti_webmgr']['virtualenv'], 'bin', 'python')
 manage_loc = File.join(node['ganeti_webmgr']['path'], node['ganeti_webmgr']['manage_file'])
 manage_dir = File.dirname(manage_loc)
