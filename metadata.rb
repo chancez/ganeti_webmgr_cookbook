918b8845701a0cce0f6d34abc47c8cf60d0543e9
Chance Zibolski <chance.zibolski@gmail.com>
2013-10-29 12:52:16 -0700

    Bump version to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chance@osuosl.org'
 license          'All rights reserved'
 description      'Installs/Configures Ganeti Web Manager'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.2.0'
 depends          'python'
 depends          'git'
 depends          'supervisor'
