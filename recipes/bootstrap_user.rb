5e6254bc4cf32f106782bcd607013f78c823e781
Chance Zibolski <chance.zibolski@gmail.com>
2014-06-06 15:40:16 -0700

    Fix bootstrap_user

--- recipes/bootstrap_user.rb
+++ recipes/bootstrap_user.rb
@@ -17,18 +17,24 @@
 
 venv = node['ganeti_webmgr']['install_dir']
 venv_bin = ::File.join(venv, 'bin')
+python = ::File.join(venv_bin, 'python')
 django_admin = ::File.join(venv_bin, 'django-admin.py')
 
 username = node['ganeti_webmgr']['admin_username'] || 'admin'
 password = node['ganeti_webmgr']['admin_password'] || 'password'
 email = node['ganeti_webmgr']['admin_email'] || 'admin@example.com'
 
+env = {
+  "GWM_CONFIG_DIR" => "#{node['ganeti_webmgr']['config_dir']}" ,
+  "DJANGO_SETTINGS_MODULE" => "ganeti_webmgr.ganeti_web.settings"
+}
+
 execute "bootstrap_superuser" do
   command <<-EOS
   #{django_admin} createsuperuser --noinput --username=#{username} --email #{email}
-  #{django_admin} shell -c \"from django.contrib.auth.models import User;u=User.objects.get(username='#{username}');u.set_password('#{password}');u.save();\"
+  #{python} -c \"from django.contrib.auth.models import User;u=User.objects.get(username='#{username}');u.set_password('#{password}');u.save();\"
   EOS
   user node['ganeti_webmgr']['user']
   group node['ganeti_webmgr']['group']
-  environment ({'DJANGO_SETTINGS_MODULE' => 'ganeti_webmgr.ganeti_web.settings'})
+  environment env
 end
