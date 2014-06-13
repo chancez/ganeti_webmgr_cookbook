39f9e306056371500baa1539f6ca83e0ae8cdbbd
Chance Zibolski <chance.zibolski@gmail.com>
2014-06-13 13:23:27 -0700

    Quote site_domain setting in config.yml. Also fix erb tag.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chance@osuosl.org'
 license          'All rights reserved'
 description      'Installs/Configures Ganeti Web Manager'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.5'
+version          '0.4.6'
 depends          'python'
 depends          'git'
 depends          'nginx'
