f61806c26cdd32030c7cd68ae25a55e84c81c04f
Evan Tschuy <tschuy@osuosl.org>
2014-07-09 10:36:00 -0700

    Remove unnecessary code and Rubyify

--- test/integration/default/serverspec/vncauthproxy_daemon_spec.rb
+++ test/integration/default/serverspec/vncauthproxy_daemon_spec.rb
@@ -1,13 +1,6 @@
 require 'serverspec'
 
 include Serverspec::Helper::Exec
-include Serverspec::Helper::DetectOS
-
-RSpec.configure do |c|
-  c.before :all do
-    c.path = '/sbin:/usr/sbin'
-  end
-end
 
 describe "install and start the vncauthproxy daemon" do
   describe service("vncauthproxy") do
@@ -15,7 +8,7 @@ describe "install and start the vncauthproxy daemon" do
   end
   # this will fail if you change the port
   # in the attributes
-  it "is listening on port 8888" do
-    expect(port(8888)).to be_listening
+  describe port(8888) do
+    it { should be_listening }
   end
 end
