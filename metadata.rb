584a748712e0fa252cdc2439d1e7c27855baae35
Evan Tschuy <tschuy@osuosl.org>
2015-04-27 13:52:48 -0700

    Bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chance@osuosl.org'
 license          'All rights reserved'
 description      'Installs/Configures Ganeti Web Manager'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.4.9'
+version          '0.4.10'
 depends          'build-essential'
 depends          'python'
 depends          'git'
